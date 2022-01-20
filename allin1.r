fead1c35704294c41646b34a1cf4681f98ea184b
esslamb <amelia1223.ess@gmail.com>
2022-01-20 17:31:48 +0800

    use get symbol

--- allin1.r
+++ allin1.r
@@ -2,25 +2,26 @@ library(PerformanceAnalytics)
 library(quantmod)
 library(stats)
 library(xts)
+library(dplyr)
 
-df <- read.csv(file='/Users/angal/OneDrive/Documents/la/all.csv')
 
 tar_and_ind=c('VOO','AAPL', 'AMZN', 'BRK-B', 'FB', 'GOOG', 'GOOGL', 'JNJ', 'JPM', 'MSFT', 'NVDA', 'TSLA')
 
 #calculate return of asset
-dfx = xts(df$VOO, order.by=as.Date(df$Date))
-re <- Return.calculate(dfx, method = c("discrete", "log"))
-rea= Return.annualized(re, scale = 252, geometric = TRUE)
+dfx= getSymbols('VOO', src = 'yahoo', from = "2016-12-30", to = "2021-12-31", auto.assign = FALSE)
+re <- Return.calculate(Ad(dfx), method = c("discrete", "log"))
+rea= Return.annualized(re, scale = NA, geometric = TRUE)
 colnames(re) <- 'VOO'
 colnames(rea) <- 'VOO'
+re=na.omit(re)
 head(re)
 head(rea)
 
-symbols=c('AAPL', 'AMZN', 'BRK-B', 'FB','GOOG')
+symbols=c('AAPL', 'AMZN', 'BRK-B', 'FB','GOOG', 'GOOGL', 'JNJ', 'JPM', 'MSFT', 'NVDA', 'TSLA')
 
 #calculate the returns of multiple indexes
 for(i in 1:length(symbols)) {
-  sym <- getSymbols(symbols[i], src = 'yahoo',from = "2015-01-01", to = "2021-12-25", auto.assign = FALSE)
+  sym <- getSymbols(symbols[i], src = 'yahoo',from = "2016-12-30", to = "2021-12-31", auto.assign = FALSE)
   returns <- Return.calculate(Ad(sym), method = c("discrete", "log"))
   return_a= Return.annualized(returns, scale = NA, geometric = TRUE)
   colnames(returns) <- symbols[i]
@@ -28,7 +29,8 @@ for(i in 1:length(symbols)) {
   re=merge(re, returns)
   rea=merge(rea, return_a)
 }
+re=na.omit(re)
 head(re)
 head(rea)
 
-#fffff
+

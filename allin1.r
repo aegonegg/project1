6bb5f70672786975ea0116ab346c3ccf6463f098
esslamb <amelia1223.ess@gmail.com>
2022-01-20 17:53:35 +0800

    weird

--- allin1.r
+++ allin1.r
@@ -3,22 +3,27 @@ library(quantmod)
 library(stats)
 library(xts)
 library(dplyr)
+library(TTR)
 
+library(rugarch)
+library(forecast)
+library(ggplot2)
 
 tar_and_ind=c('VOO','AAPL', 'AMZN', 'BRK-B', 'FB', 'GOOG', 'GOOGL', 'JNJ', 'JPM', 'MSFT', 'NVDA', 'TSLA')
 
 #calculate return of asset
 dfx= getSymbols('VOO', src = 'yahoo', from = "2016-12-30", to = "2021-12-31", auto.assign = FALSE)
-re <- Return.calculate(Ad(dfx), method = c("discrete", "log"))
-rea= Return.annualized(re, scale = NA, geometric = TRUE)
-colnames(re) <- 'VOO'
-colnames(rea) <- 'VOO'
-re=na.omit(re)
-head(re)
-head(rea)
+re1 <- Return.calculate(Ad(dfx), method = c("discrete", "log"))
+rea1 <- Return.annualized(re1, scale = NA, geometric = TRUE)
+colnames(re1) <- 'VOO'
+colnames(rea1) <- 'VOO'
+re1=na.omit(re1)
+head(re1)
+head(rea1)
 
 symbols=c('AAPL', 'AMZN', 'BRK-B', 'FB','GOOG', 'GOOGL', 'JNJ', 'JPM', 'MSFT', 'NVDA', 'TSLA')
-
+re=re1
+rea=rea1
 #calculate the returns of multiple indexes
 for(i in 1:length(symbols)) {
   sym <- getSymbols(symbols[i], src = 'yahoo',from = "2016-12-30", to = "2021-12-31", auto.assign = FALSE)
